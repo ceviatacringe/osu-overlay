316baa2242113143d524a1769e17ee30674de19e
Alex <revellxd@gmail.com>
2024-02-26 14:40:50 +0200

    Reverting

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from get_map_ID import get_map_macro
 HOTKEY = 'f4'
 TIME_TO_SLEEP = 0.03
 TAB_LOADING_TIME = 0.2
-# This will automatically minimize the browser after opening a map
-# To enable, put in your own path, to disable, set to: None
-BROWSER_TO_MINIMIZE = "Google Chrome"
-
 
 def main():
     # Main function to run the application loop
     while True:
         # Get map details and prepare initialization
-        get_map_macro(TIME_TO_SLEEP, TAB_LOADING_TIME, HOTKEY, BROWSER_TO_MINIMIZE)
+        get_map_macro(TIME_TO_SLEEP, TAB_LOADING_TIME, HOTKEY)
         overlay = OsuOverlay()
         # Run the canvas and circle rendering script
         overlay.initialize_script()
